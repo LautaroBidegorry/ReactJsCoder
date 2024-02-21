c1556be75ff51cdd550886b29e29e486a1b0aa0d
Lautaro Bidegorry <bidegorrylb1@gmail.com>
2024-02-21 20:49:54 -0300

    pe2cmock

--- src/components/asyncMock.js
+++ src/components/asyncMock.js
@@ -22,8 +22,55 @@ const products = [
         stock: '20',
         price: '$300',
         description: 'descripcion producto 3'
+    },
+    {
+        id: '4',
+        name: 'producto 4',
+        category: 'c1',
+        stock: '10',
+        price: '$100',
+        description: 'descripcion producto 4'
+    },
+    {
+        id: '5',
+        name: 'producto 5',
+        category: 'c2',
+        stock: '15',
+        price: '$200',
+        description: 'descripcion producto 5'
+    },
+    {
+        id: '6',
+        name: 'producto 6',
+        category: 'c3',
+        stock: '20',
+        price: '$300',
+        description: 'descripcion producto 6'
+    },
+    {
+        id: '7',
+        name: 'producto 7',
+        category: 'c1',
+        stock: '10',
+        price: '$100',
+        description: 'descripcion producto 7'
+    },
+    {
+        id: '8',
+        name: 'producto 8',
+        category: 'c2',
+        stock: '15',
+        price: '$200',
+        description: 'descripcion producto 8'
+    },
+    {
+        id: '9',
+        name: 'producto 9',
+        category: 'c3',
+        stock: '20',
+        price: '$300',
+        description: 'descripcion producto 9'
     }
-
 ]
 
 export const getProducts = () => {
